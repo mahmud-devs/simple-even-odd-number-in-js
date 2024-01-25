b931b177358a3eb5cce9f9f59284d0f0387ef9c0
mahmud-devs <mahmudulislam0999@gmail.com>
2024-01-25 18:26:42 +0600

    data type input fixed

--- js/main.js
+++ js/main.js
@@ -7,9 +7,9 @@
 let input = prompt("Enter any number");
 let number = +input;
 
-if (number % 2 == 0) {
+if (number % 2 == 0 && number >= 0) {
     console.log(number,"is a even number" );
-} else if(number % 2 !== 0){
+} else if(number % 2 !== 0 && number >= 0){
     console.log(number, "is a odd number");
 }
  else {
