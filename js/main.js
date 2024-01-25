8ea7ae29d15db595df01bda9476ddf7e9ef9d031
mahmud-devs <mahmudulislam0999@gmail.com>
2024-01-25 18:02:23 +0600

    even and odd number

---
+++ js/main.js
@@ -0,0 +1,17 @@
+/*====================================
+
+    odd and even number
+
+=====================================*/
+
+let input = prompt("Enter any number");
+let number = +input;
+
+if (number % 2 == 0) {
+    console.log(number,"is a even number" );
+} else if(number % 2 !== 0){
+    console.log(number, "is a odd number");
+}
+ else {
+    console.log("invalid input");
+}
